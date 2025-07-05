494b7ca5595dee463c935676eaa3c598612963df
Alex Vega <axvg@users.noreply.github.com>
2025-07-05 14:24:44 -0500

    fix lint

--- lint.py
+++ lint.py
@@ -1,4 +1,6 @@
-import subprocess, sys
+import subprocess
+import sys
+
 result = subprocess.run(["flake8", "."], capture_output=True, text=True)
 if result.returncode != 0:
     print("Errores de estilo encontrados:", file=sys.stderr)
